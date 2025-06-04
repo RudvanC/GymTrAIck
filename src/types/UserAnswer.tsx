49e2433ff8476e3669aea012fa8c19e895420d43
Rudvan <rudvancastro@gmail.com>
2025-06-04 12:03:58 +0200

    refactor(Dashboard): move UserAnswer interface to a separate file for better organization

---
+++ src/types/UserAnswer.tsx
@@ -0,0 +1,11 @@
+export interface UserAnswer {
+  id: string;
+  created_at: string;
+  training_experience: string;
+  availability: string;
+  injuries: string;
+  equipment_access: boolean;
+  goal: string;
+  fitness_level: string;
+  session_duration: string;
+}
