6f674e8ccd8d0979eabc927ecd289ba7d31c57ac
Rudvan <rudvancastro@gmail.com>
2025-06-13 10:26:43 +0200

    feat: remove NavbarAuth component from ExercisesPage for cleaner layout

--- src/app/routine/page.tsx
+++ src/app/routine/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 import { RoutineList } from "@/components/routine/RoutineList";
-import NavbarAuth from "@/components/layout/NavbarAuth";
 function ExercisesPage() {
   return (
     <>
-      <NavbarAuth />
       <div className="max-w-7xl mx-auto p-8">
         <RoutineList />
       </div>
