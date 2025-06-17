9d5a43490128f3fe6b47bd5ac3fde0a722cfc3d0
Rudvan <rudvancastro@gmail.com>
2025-06-17 09:21:40 +0200

    feat: set up Jest for testing with environment variables and update package scripts

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  /** Opcional: alias “@/” de tu proyecto Next */
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  setupFiles: ["<rootDir>/jest.setup.ts"], // para variables de entorno
+};
