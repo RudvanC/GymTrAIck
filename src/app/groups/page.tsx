177c2b651e99ba5d1b289c98efa0ac5d08b42ac3
Rudvan <rudvancastro@gmail.com>
2025-06-19 19:44:30 +0200

    feat: integrate GlobalChat component into GroupsPage and remove Accordion component

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import GlobalChat from "@/app/groups/components/GlobalChat";
+
 export default function GroupsPage() {
-  return <div className="bg-slate-950 text-slate-50 min-h-screen"></div>;
+  return (
+    <div className="bg-slate-950 text-slate-50 min-h-screen">
+      <GlobalChat />
+    </div>
+  );
 }
