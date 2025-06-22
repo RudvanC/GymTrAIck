7b6cd8af649dcdc98f5c4d46303f6824e6b8291f
Rudvan <rudvancastro@gmail.com>
2025-06-22 16:48:50 +0200

    refactor: improve layout structure and remove unnecessary min-h-screen classes in DashboardLayout components

--- src/app/progress/layout.tsx
+++ src/app/progress/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-slate-950 text-slate-50 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] flex pb-20 md:pb-0">
+    <div className="flex-grow bg-slate-950 text-slate-50 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] flex pb-20 md:pb-0">
       <Sidebar />
       <main className="flex-1">{children}</main>
     </div>
